9b8a074729572cd9b79453ca7bf965e424ec4086
roothunter <antonio.ricciardi99@outlook.it>
2024-11-16 17:50:39 +0100

    stable bro

--- src/engine.rs
+++ src/engine.rs
@@ -1,245 +1,65 @@
 use rustc_hash::FxHashMap;
 
-enum Error {
-    CantAddElement
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
+pub struct Data
+{
+    pub length: usize,
+    tokens: FxHashMap<String, Vec<u32>>,
+    extra_separators: FxHashMap<usize, u8>,
 }
 
-pub trait EngineAllowedTypes{} 
-impl EngineAllowedTypes for u8 {}
-impl EngineAllowedTypes for String {}
-
-pub trait Engine<T> 
-where 
-    T: EngineAllowedTypes + serde::Deserialize<'static> + std::cmp::Eq + std::hash::Hash,
+impl Data
 {
-    fn new(size: usize) -> Data<T> {
-        return Data { size, dict: FxHashMap::<T, Vec<DataElement>>::default(), data_length: 0 };
+    pub fn new() -> Data {
+        return Data{length: 0, tokens: FxHashMap::default(), extra_separators: FxHashMap::default()};
     }
 
-    fn from_slice_u8(buffer: &[T]) -> Self;
-    fn from_slice_vec(buffer: Vec<T>) -> Self;
+    pub fn add_element(&mut self, s: &String) {
+        let index = self.length as u32;
 
-    fn add_element(&mut self, s: &T);
-
-    fn to_str(self) -> String;
-    fn to_bytes(self) -> Vec<u8>;
-
-    fn exists_elem_by_index(&mut self, key: T, index: usize) -> bool;
-}
-
-pub trait AllowedExportTypes {}
-
-impl AllowedExportTypes for u8 {}
-impl AllowedExportTypes for u16 {}
-impl AllowedExportTypes for usize {}
-
-#[derive(serde::Serialize, serde::Deserialize, Debug)]
-enum DataExportType {
-    U8,
-    U16,
-    USIZE
-}
-
-pub trait ExportBehavior<T, D, E> 
-where 
-    T: EngineAllowedTypes + std::cmp::Eq + std::hash::Hash,
-    D: AllowedExportTypes + std::cmp::Eq + std::hash::Hash,
-    E: AllowedExportTypes + std::cmp::Eq + std::hash::Hash,
-{
-    //fn new(size: usize, dict: HashMap<T, Vec<usize>>, elements: Vec<E>) -> DataExport<T, D, E>;
-    fn to_bytes(self) -> Vec<u8>;
-}
-
-#[derive(serde::Serialize, serde::Deserialize, Debug)]
-pub struct DataExport<T, D, E>
-where 
-    T: EngineAllowedTypes + std::cmp::Eq + std::hash::Hash,
-    D: AllowedExportTypes + std::cmp::Eq + std::hash::Hash,
-    E: AllowedExportTypes + std::cmp::Eq + std::hash::Hash,
-{
-    dict_type: DataExportType,
-    elements_type: DataExportType,
-    size: usize,
-    dict: FxHashMap<T, Vec<D>>,
-    elements: Vec<E>
-}
-
-impl ExportBehavior<String, u16, u8> for DataExport<String, u16, u8> {
-    // fn new(size: usize, dict: HashMap<String, Vec<usize>>) -> DataExport<String, u16, u8> {
-    //     let mut dictionary = HashMap::<String, Vec<u16>>::new();
-        
-    //     for key in dict.keys() {
-    //         let value = dict.get(key).unwrap();
-    //         let mut v = Vec::<u16>::new();
-
-    //         for e in value {
-    //             let value = *e as u16;
-    //             v.push(value);
-    //         }
-
-    //         dictionary.insert(key.clone(), v);
-    //     }
-        
-    //     return DataExport{
-    //         dict: dictionary,
-    //         size,
-    //         elements,
-    //         dict_type: DataExportType::U16,
-    //         elements_type: DataExportType::U8,
-    //     };
-    // }
- 
-    fn to_bytes(self) -> Vec<u8> {
-        return bincode::serialize(&self).unwrap();
-    }
-}
-
-#[derive(serde::Serialize, serde::Deserialize, Debug, Clone, Copy)]
-pub struct DataElement
-{
-    index: usize,
-    count: usize
-}
-
-impl DataElement {
-    pub fn inc(&mut self) {
-        self.count += 1;
-    }    
-}
-
-#[derive(serde::Serialize, serde::Deserialize, Debug)]
-pub struct Data<T>
-where 
-    T: EngineAllowedTypes + std::cmp::Eq + std::hash::Hash,
-{
-    pub size: usize,
-    pub data_length: usize,
-    dict: FxHashMap<T, Vec<DataElement>>,
-}
-
-impl Engine<u8> for Data<u8>
- {
-    fn from_slice_u8(buffer: &[u8]) -> Self {
-        return Data { size: buffer.len(), dict: FxHashMap::default(), data_length: 0 };
-    }
-
-    fn from_slice_vec(buffer: Vec<u8>) -> Self {
-        return Data { size: buffer.len(), dict: FxHashMap::default(), data_length: 0 };
-    }
-
-    fn exists_elem_by_index(&mut self, key: u8, index: usize) -> bool {
-        let symbol_elements = self.dict.get(&key).unwrap();
-
-        for elem in symbol_elements {
-            if elem.index == index {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    fn add_element(&mut self, s: &u8) {
-        let index = self.data_length;
-
-        let exists = self.exists_elem_by_index(*s, index);
-        if exists {
-            let elem = self.dict.get_mut(s).unwrap().get_mut(index).unwrap();
-            elem.inc();
-            
-            self.data_length += 1;
+        if self.tokens.contains_key(s) {
+            let elem = self.tokens.get_mut(s).unwrap();
+            elem.push(index);
         } else {
-            let elem = DataElement{
-                index,
-                count: 1
-            };
-
-            self.dict.insert(*s, Vec::from([elem]));
+            self.tokens.insert(s.clone(), Vec::from([index]));
         }
+        self.length += 1;
     }
 
-    fn to_bytes(self) -> Vec<u8> {
+    pub fn add_separator(&mut self, count: usize) {
+        let index = self.length;
+        self.extra_separators.insert(index, count as u8);
+        self.length += count;
+    }
+
+    pub fn compress(self) -> Vec<u8> {
+        //println!("COMPRESS DATA: {:?}", self);
+
         return bincode::serialize(&self).unwrap();
     }
-    
-    fn to_str(self) -> String {
-        let mut output = Vec::<u8>::new();
-        output.reserve(self.size);
 
-        for key in self.dict.keys() {
-            let elements = self.dict.get(key).unwrap();
-            for element in elements {
-                let e = output.get_mut(element.index).unwrap();
-                *e = *key;
-            }
-        }
+    pub fn decompress(self) -> String {
+        let mut data = Vec::<String>::new();
+        data.reserve(self.length);
 
-        return String::from_utf8(output).unwrap();
-    }
-}
-
-impl Engine<String> for Data<String>
- {
-    fn from_slice_u8(buffer: &[String]) -> Self {
-        return Data { size: buffer.len(), dict: FxHashMap::default(), data_length: 0 };
-    }
-
-    fn from_slice_vec(buffer: Vec<String>) -> Self {
-        return Data { size: buffer.len(), dict: FxHashMap::default(), data_length: 0 };
-    }
-
-    fn add_element(&mut self, s: &String) {
-        let index = self.data_length;
-
-        if self.exists_elem_by_index(s.clone(), index) {
-            if let Some(elements) = self.dict.get_mut(s) {
-                if let Some(elem) = elements.get_mut(index) {
-                    elem.inc();
-                } else {
-                    println!("ERROR ELEMENT");
+        for i in 0..self.length {
+            let mut flag = false;
+                for (k, v) in self.tokens.clone() {
+                    for index in v {
+                        if i == index as usize {
+                            data.push(k);
+                            flag = true;
+                            break;
+                        } 
+                    }
+                    if flag {
+                        break;
+                    }
                 }
-            } else {
-                println!("ERROR ELEMENTS");
-            }
-            
-        } else {
-            let elem = DataElement{
-                index,
-                count: 1
-            };
-
-            self.dict.insert(s.clone(), Vec::from([elem]));
-        }
-    }
-
-    fn to_bytes(self) -> Vec<u8> {
-        return bincode::serialize(&self).unwrap();
-    }
-    
-    fn to_str(self) -> String {
-        let mut output = Vec::<String>::new();
-        output.reserve(self.size);
-
-        for key in self.dict.keys() {
-            let elements = self.dict.get(key).unwrap();
-            for element in elements {
-                let e = output.get_mut(element.index).unwrap();
-                *e = key.clone();
-            }
         }
 
-        return output.join("");
-    }
-    
-    fn exists_elem_by_index(&mut self, key: String, index: usize) -> bool {
-        
-        if let Some(symbol_elements) = self.dict.get(&key) {
-            for elem in symbol_elements {
-                if elem.index == index {
-                    return true;
-                }
-            }
-        }
-
-        return false;
+        println!("TOKENS: {}", self.length);
+        //println!("DECOMPRESS DATA: {:?}", self);
+        return data.join(" ");
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,44 @@
-use engine::{Data, Engine};
+use engine::{Data};
 
 pub mod engine;
 
-pub fn compress(buf: &[u8]) {
-    let mut data = Data::<String>::new(buf.len());
-
-    let str = std::str::from_utf8(buf).unwrap().split(' ');
+pub fn compress(path: &str, buf: &[u8]) {
+    let mut data = Data::new();
+    let str = std::str::from_utf8(buf).unwrap();
     
-    for part in str {
-        let part = part.to_string();
-        data.add_element(&part);
+    let mut token = Vec::<u16>::new();
+    let mut sep_count = 0;
+
+    for c in str.chars() {
+        if c != ' ' {
+            if sep_count > 1 {
+                data.add_separator(sep_count);
+                sep_count = 0;
+            }
+            
+            token.push(c as u16);
+        } else {
+            if token.len() > 0 {
+                let part = String::from_utf16(token.as_slice()).unwrap();
+                data.add_element(&part);
+                token.clear();
+                sep_count = 0;
+            }
+            sep_count += 1;
+        }
     }
 
-    println!("Original length: {}", data.size);
-    let encoded = data.to_bytes();
+    println!("Original length: {}", buf.len());
+    let encoded = data.compress();
     println!("Compress length: {}", encoded.len());
+
+    std::fs::write(path, encoded).unwrap();
+
 }
 
-pub fn decompress(buf: &[u8]) {
-    
+pub fn decompress(buf: &[u8]) -> String {
+    let data: Data = bincode::deserialize(buf).unwrap();
+    return data.decompress();
 }
 
 #[cfg(test)]
@@ -26,10 +46,28 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-
-        let test_path = "/home/roothunter/Dev/ran/files/text1.txt";
+    fn decompress_1() {
+        let test_path = "/home/roothunter/Dev/ran/files/text_1.txt";
+        let compress_path = "/home/roothunter/Dev/ran/files/compress_2.ran";
+        let decompress_path = "/home/roothunter/Dev/ran/files/decompress_2.ran";
+        
         let test = std::fs::read(test_path).unwrap();
-        compress(test.as_slice());
+        compress(compress_path, test.as_slice());
+
+        let compress_data = std::fs::read(compress_path).unwrap();
+        let compress_data = compress_data.as_slice();
+
+        let decompressed = decompress(&compress_data);
+
+        std::fs::write(decompress_path, decompressed).unwrap();
+    }
+
+    #[test]
+    fn compress_1() {
+        let test_path = "/home/roothunter/Dev/ran/files/text_1.txt";
+        let compress_path = "/home/roothunter/Dev/ran/files/compress_1.ran";
+        
+        let test = std::fs::read(test_path).unwrap();
+        compress(compress_path, test.as_slice());
     }
 }
