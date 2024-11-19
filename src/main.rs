d7fa476481108f9ad28adf427cc31591468fee37
roothunter <antonio.ricciardi99@outlook.it>
2024-11-19 09:33:07 +0100

    stable + fix bugs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::collections::{BTreeMap, HashMap};
+use std::collections::HashMap;
 use priority_queue::{self, PriorityQueue};
-use serde::de::value;
 use std::cmp::Reverse;
 
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -12,31 +11,27 @@ struct Node {
     pub right: Option<Box<Node>>,
 }
 
-struct DataV2 {
-    dict: BTreeMap<usize, Node>
-}
-
-fn build_btree(node: Node, dict: &mut HashMap<Box<Vec<u8>>, char>, acc: Box<Vec<u8>>, depth: usize) {
+fn build_btree(node: Node, dict: &mut HashMap<char, Vec<u8>>, acc: Vec<u8>, depth: usize) {
     if node.left == None && node.right == None {
         //let key = format!("{}{}", acc, bit);
 
-        dict.insert(acc, node.symbol.unwrap());
+        dict.insert(node.symbol.unwrap(), acc.clone());
     } else {
         let left = node.left.unwrap();
         let right = node.right.unwrap();
 
         if !node.is_root && depth > 0 {
-            let mut acc_left = *acc.clone();
+            let mut acc_left = acc.clone();
             acc_left.push(0);
 
-            let mut acc_rigth = *acc.clone();
+            let mut acc_rigth = acc.clone();
             acc_rigth.push(1);
 
-            build_btree(*left, dict, Box::new(acc_left), depth + 1);
-            build_btree(*right, dict, Box::new(acc_rigth), depth + 1);
+            build_btree(*left, dict, acc_left, depth + 1);
+            build_btree(*right, dict, acc_rigth, depth + 1);
         } else {
-            let acc_left = Box::new(Vec::from([0]));
-            let acc_rigth = Box::new(Vec::from([1]));
+            let acc_left = Vec::from([0]);
+            let acc_rigth = Vec::from([1]);
 
             build_btree(*left, dict, acc_left, 1);
             build_btree(*right, dict, acc_rigth, 1);
@@ -45,10 +40,7 @@ fn build_btree(node: Node, dict: &mut HashMap<Box<Vec<u8>>, char>, acc: Box<Vec<
 }
 
 fn main() {
-    let data = BTreeMap::<u16, char>::new();
-
-
-    let input_string = "AABBCDV dsad sad daaaaaaskm mmmf dkwqe qqwmm kmz z zm kmdask dmm ";
+    let input_string = "Ciao a tutti mi chiamo Antonio e questa è la prima prova di compressione usando il metodo di Huffman";
 
     let chars = input_string.chars();
 
@@ -64,7 +56,7 @@ fn main() {
         }
     }
     
-    assert!(distribution.values().sum::<usize>() == chars.count());
+    assert!(distribution.values().sum::<usize>() == chars.clone().count());
     println!("DIST: {:?}", distribution);
 
     let mut pq = PriorityQueue::new();
@@ -106,14 +98,49 @@ fn main() {
 
     let (root, _) = pq.pop().unwrap();
 
-    let mut dict = HashMap::<Box<Vec<u8>>, char>::new();
-    let acc = Box::new(Vec::new());
+    let mut conversion_dict = HashMap::<char, Vec<u8>>::new();
+    let acc = Vec::new();
 
-    build_btree(root, &mut dict, acc, 0);
+    build_btree(root, &mut conversion_dict, acc, 0);
 
-    for (k, v) in &dict {
-        println!("{}:{:?}", v, *k);
+    println!("CONVERSION HASH: {:?}", conversion_dict);
+
+    let mut compressed: Vec<u8> = Vec::new();
+
+    for c in chars.clone() {
+        println!("{} -> {:?}", c, conversion_dict.get(&c.clone()));
+        let comp = conversion_dict.get(&c.clone()).unwrap().clone();
+
+        for d in comp {
+            compressed.push(d);
+        }
     }
 
-    println!("HASH: {:?}", dict);
+    println!("COMPRESSED: {:?}", compressed);
+    println!("ORIGINAL LENGTH: {}", chars.count());
+    println!("COMPRESSED LENGTH: {}", compressed.len() / 8);
+
+
+    let chunks = compressed.chunks(8);
+
+    let mut output: Vec<u8> = Vec::new();
+
+    for chunk in chunks {
+        let mut b: u8 = 0x00;
+
+        let mut position: usize = 0;
+        for bit in chunk {
+            if *bit == 1 {
+                b = b | 1 << 7 - position;
+            } else {
+                b = b | 0 << 7 - position;
+            }
+            
+            position += 1;
+        }
+        output.push(b);
+        println!("COMPRESSED CHUNK ({:08b}): {:?}", b, chunk);
+    }
+
+    println!("OUTPUT: {:?}", output);
 }
