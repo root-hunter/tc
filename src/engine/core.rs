a4541a4af9075f229b38433215ae7b37a629096c
roothunter <antonio.ricciardi99@outlook.it>
2024-11-23 10:10:27 +0100

    v2

--- src/engine/core.rs
+++ src/engine/core.rs
@@ -6,20 +6,32 @@ use std::collections::HashMap;
     serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash,
 )]
 pub struct Node {
-    pub is_root: bool,
-    pub freq: usize,
     pub symbol: Option<char>,
     pub left: Option<Box<Node>>,
     pub right: Option<Box<Node>>,
+
+    #[serde(skip)]
+    pub freq: usize,
 }
 
-pub fn build_btree(node: Node, dict: &mut HashMap<char, Vec<u8>>, acc: Vec<u8>, depth: usize) {
-    println!("##########################################");
-    if depth == 0 {
-        println!("ROOT: {:?}\nDEPTH: {}\nACC: {:?}\nMAP: {:?}",  node, depth, acc, dict);
-    } else {
-        println!("NODE: {:?}\nDEPTH: {}\nACC: {:?}\nMAP: {:?}", node, depth, acc, dict);
-    }
+
+#[derive(
+    serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash,
+)]
+pub struct Data {
+    pub length: usize,
+    pub dict: Node,
+    pub data: Vec<u8>,
+}
+
+
+pub fn build_dict(node: Node, dict: &mut HashMap<char, Vec<u8>>, acc: Vec<u8>, depth: usize, is_root: bool) {
+    // println!("##########################################");
+    // if depth == 0 {
+    //     println!("ROOT: {:?}\nDEPTH: {}\nACC: {:?}\nMAP: {:?}",  node, depth, acc, dict);
+    // } else {
+    //     println!("NODE: {:?}\nDEPTH: {}\nACC: {:?}\nMAP: {:?}", node, depth, acc, dict);
+    // }
     
     if node.left == None && node.right == None && node.symbol.is_some() {
         dict.insert(node.symbol.unwrap(), acc.clone());
@@ -27,42 +39,42 @@ pub fn build_btree(node: Node, dict: &mut HashMap<char, Vec<u8>>, acc: Vec<u8>, 
         let left = node.left.unwrap();
         let right = node.right.unwrap();
 
-        if !node.is_root && depth > 0 {
+        if !is_root && depth > 0 {
             let mut acc_left = Vec::from(acc.clone());
             acc_left.push(0);
 
             let mut acc_rigth = Vec::from(acc.clone());
             acc_rigth.push(1);
 
-            build_btree(*left, dict, acc_left, depth + 1);
-            build_btree(*right, dict, acc_rigth, depth + 1);
+            build_dict(*left, dict, acc_left, depth + 1, false);
+            build_dict(*right, dict, acc_rigth, depth + 1, false);
         } else {
             let acc_left = Vec::from([0]);
             let acc_rigth = Vec::from([1]);
 
-            build_btree(*left, dict, acc_left,1);
-            build_btree(*right, dict, acc_rigth, 1);
+            build_dict(*left, dict, acc_left,1, false);
+            build_dict(*right, dict, acc_rigth, 1, false);
         }
 
     }
 }
 
-pub fn find_symbol(node: Node, sequence: Vec<u8>, depth: usize) -> Option<char> {
+pub fn find_symbol(dict: Node, sequence: Vec<u8>, depth: usize) -> Option<char> {
     //println!("SEQ: {:?}", sequence);
     // println!("ACC: {:?}", sequence);
     // println!("DEPTH: {:?}", depth);
     // println!("{:?}", node);
 
     if depth == 0 {
-        if node.symbol.is_some() {
-            return Some(node.symbol.unwrap());
+        if dict.symbol.is_some() {
+            return Some(dict.symbol.unwrap());
         } else {
             return None;
         }
     } else {
-        if node.left != None && node.right != None {
-            let left = node.left.unwrap();
-            let right = node.right.unwrap();
+        if dict.left != None && dict.right != None {
+            let left = dict.left.unwrap();
+            let right = dict.right.unwrap();
 
             let first = sequence.get(sequence.len() - depth).unwrap();
             //println!("FIRST {}", first);
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
 use std::{cmp::Reverse, collections::HashMap};
 
-use engine::core::{build_btree, Node};
+use engine::core::{build_dict, find_symbol, Data, Node};
 use priority_queue::PriorityQueue;
 
 pub mod engine {
     pub mod core;
 }
 
-pub fn compress(buf: &[u8]) -> Vec<u8> {
-    let input_string: String = String::from_utf8(buf.to_vec()).unwrap();
+pub fn compress(input_string: String) -> Data {
     let chars = input_string.chars();
 
     let mut distribution = HashMap::<char, usize>::new();
@@ -24,7 +23,7 @@ pub fn compress(buf: &[u8]) -> Vec<u8> {
     }
 
     assert!(distribution.values().sum::<usize>() == chars.clone().count());
-    println!("DIST: {:?}", distribution);
+    //println!("DIST: {:?}", distribution);
 
     let mut pq = PriorityQueue::new();
 
@@ -35,7 +34,6 @@ pub fn compress(buf: &[u8]) -> Vec<u8> {
                 symbol: Some(k),
                 left: None,
                 right: None,
-                is_root: false,
             },
             Reverse(v),
         );
@@ -54,42 +52,68 @@ pub fn compress(buf: &[u8]) -> Vec<u8> {
             symbol: None,
             left: Some(Box::new(left)),
             right: Some(Box::new(right)),
-            is_root: false,
         };
 
         pq.push(node, Reverse(freq));
     }
 
-    let (mut root, _) = pq.pop().unwrap();
-    root.is_root = true;
-    println!("{:?}", root);
+    //println!("{:#?}", pq);
+
+    let (root, _) = pq.pop().unwrap();
+    //println!("{:?}", root);
 
     let mut conversion_dict = HashMap::<char, Vec<u8>>::new();
     let acc = Vec::new();
 
-    build_btree(root.clone(), &mut conversion_dict, acc, 0);
-
-    println!("CONVERSION HASH: {:?}", conversion_dict);
-
+    build_dict(root.clone(), &mut conversion_dict, acc, 0, true);
     let mut compressed: Vec<u8> = Vec::new();
 
     for c in chars.clone() {
-        println!("{} -> {:?}", c, conversion_dict.get(&c.clone()));
+        //println!("{} -> {:?}", c, conversion_dict.get(&c.clone()));
         let comp = conversion_dict.get(&c.clone()).unwrap().clone();
 
         for d in comp {
             compressed.push(d);
         }
     }
+    let compressed_len = compressed.len();
 
-    return compressed;
+    let chunks = compressed.chunks(8);
+
+    let mut output: Vec<u8> = Vec::new();
+
+    for chunk in chunks {
+        let mut b: u8 = 0x00;
+
+        let mut position: usize = 0;
+        for bit in chunk {
+            if *bit == 1 {
+                b = b | 1 << 7 - position;
+            } else {
+                b = b | 0 << 7 - position;
+            }
+
+            position += 1;
+        }
+        output.push(b);
+        //println!("COMPRESSED CHUNK ({:08b}): {:?}", b, chunk);
+    }
+
+    return Data{
+        length: compressed_len,
+        dict: root,
+        data: output
+    };
 }
 
 pub fn compress_file(file_path: &str, output_path: &str) -> Result<(), ()>{
     if let Ok(file_data) = std::fs::read(file_path){
-        let data = compress(file_data.as_slice());
+        let input_string = String::from_utf8(file_data).unwrap();
 
-        if let Ok(_) = std::fs::write(output_path, data) {
+        let data = compress(input_string);
+        let bytes = bincode::serialize(&data).unwrap();
+
+        if let Ok(_) = std::fs::write(output_path, bytes) {
             Ok(())
         } else {
             Err(())
@@ -99,13 +123,50 @@ pub fn compress_file(file_path: &str, output_path: &str) -> Result<(), ()>{
     }
 }
 
-pub fn decompress(bytes: &[u8]) -> Vec<u8> {
-    return Vec::new();
+pub fn decompress(data: Data) -> String {
+    let mut bits = Vec::with_capacity(data.length);
+
+    let mut k = 0;
+    let mut end = false;
+    for byte in data.data {
+        for i in (0..8).rev() { 
+            bits.push((byte >> i) & 1);
+
+            k += 1;
+
+            if k >= data.length {
+                end = true;
+                break;
+            }
+        }
+
+        if end {
+            break;
+        }
+    }
+
+    let mut buffer = Vec::<u8>::new();
+    let mut output_string = String::new();
+    
+    for cc in bits.clone() {
+        //println!("C: {}", cc);
+        buffer.push(cc);
+        if buffer.len() > 0 {
+            if let Some(char) = find_symbol(data.dict.clone(), buffer.clone(), buffer.len()) {
+                output_string.push(char);
+                buffer.clear();
+            }
+        }
+    }
+
+    return output_string;
 }
 
 pub fn decompress_file(file_path: &str, output_path: &str) -> Result<(), ()>{
     if let Ok(file_data) = std::fs::read(file_path) {
-        let data = decompress(file_data.as_slice());
+        let data = bincode::deserialize(&file_data).unwrap();
+        let data = decompress(data);
+
         if let Ok(_) = std::fs::write(output_path, data) {
             Ok(())
         } else {
